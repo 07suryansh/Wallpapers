07dfd270223a75141345c2e341cad35a673db4b4
Suryansh Singh Bisen <07suryansh@gmail.com>
2022-10-30 12:51:12 +0530

    created navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    
+    <div className='navbar'>
+       <span className='font-large span'>Wallpapers</span>
+       <span className='font-small span'>NITH</span>
+    </div>
+    
+  )
+}
